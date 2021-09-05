cb06d5b532106693f252a04e6510aa397bc7d018
Ronaldo Peres <ronaldoperes@gmail.com>
2021-09-04 22:51:23 -0300

    first commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          var str = Kata.greet();
+          Console.WriteLine(str);
+
+          var str2 = Kata.greet2();
+          Console.WriteLine(str2);
+        }
+    }
+
+    public static class Kata
+    {
+        // Write a public static method "greet" that returns "hello world!"
+
+        public static string greet() => "hello world!";
+
+        public static string greet2()
+        {
+            return "hello world!";
+        }
+    }
+}
