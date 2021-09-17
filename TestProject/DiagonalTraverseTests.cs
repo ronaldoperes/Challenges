f35a152fa42149f2e4c3dbe8acd92fe5f2f9a0dd
Ronaldo Peres <ronaldoperes@gmail.com>
2021-09-17 13:36:06 -0300

    Add Diagonal Traverse

---
+++ TestProject/DiagonalTraverseTests.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+using LC;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace TestProject
+{
+    public class DiagonalTraverseTests
+    {
+
+        [Fact]
+        public void When_Solution_IsCalled_ReturnEquivalenteArray()
+        {
+            var expected = new int[] { 1, 2, 4, 7, 5, 3, 6, 8, 9 };
+
+            int[][] matrix = new int[3][];
+            matrix[0] =  new int[3] { 1, 2, 3 };
+            matrix[1] =  new int[3] { 4, 5, 6 };
+            matrix[2] =  new int[3] { 7, 8, 9 };
+
+
+            var result = DiagonalTravese.Solution(matrix);
+
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void When_Solution_IsCalled_ReturnEquivalenteArray2()
+        {
+            var expected = new int[] { 1,2,3 };
+
+            int[][] matrix = new int[1][];
+            matrix[0] = new int[3] { 1, 2, 3 };
+
+
+            var result = DiagonalTravese.Solution(matrix);
+
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void When_Solution_IsCalled_ReturnEquivalenteArray3()
+        {
+            var expected = new int[] { 1,2,4,5,3,6 };
+
+            int[][] matrix = new int[2][];
+            matrix[0] = new int[3] { 1, 2, 3 };
+            matrix[1] = new int[3] { 4, 5, 6 };
+
+
+            var result = DiagonalTravese.Solution(matrix);
+
+            result.Should().BeEquivalentTo(expected);
+        }
+
+    }
+}
