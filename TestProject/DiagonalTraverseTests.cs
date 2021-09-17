afad2468dbeeea779acc9ebfb11c11316ff69ada
Ronaldo Peres <ronaldoperes@gmail.com>
2021-09-17 13:48:40 -0300

    Add more test

--- TestProject/DiagonalTraverseTests.cs
+++ TestProject/DiagonalTraverseTests.cs
@@ -53,5 +53,20 @@ namespace TestProject
             result.Should().BeEquivalentTo(expected);
         }
 
+        [Fact]
+        public void When_Solution_IsCalled_ReturnEquivalenteArray4()
+        {
+            var expected = new int[] { 1,2,5,6,3,4,7,8 };
+
+            int[][] matrix = new int[2][];
+            matrix[0] = new int[4] { 1, 2, 3, 4 };
+            matrix[1] = new int[4] { 5, 6, 7, 8 };
+
+
+            var result = DiagonalTravese.Solution(matrix);
+
+            result.Should().BeEquivalentTo(expected);
+        }
+
     }
 }
