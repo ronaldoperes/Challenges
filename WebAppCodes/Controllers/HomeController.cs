b7f8a28dc8b746bd0f9a68ef40e834141db7dabb
Ronaldo Peres <ronaldoperes@gmail.com>
2021-10-24 15:34:37 -0300

    Add WebApi

---
+++ WebAppCodes/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAppCodes.Etc;
+
+namespace WebAppCodes.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var s = ExLinq.BasicAggregate();
+            return Ok(s);
+        }
+
+        [HttpGet("str")]        
+        public IActionResult Index2(string str)
+        {
+            return Ok($"Now there is something different {str}");
+        }
+
+    }
+}
---
+++ WebAppCodes/Etc/ExLinq.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppCodes.Etc
+{
+    public static class ExLinq
+    {
+        public static string BasicAggregate()
+        {
+            var numbers = new[] { 1, 2, 3, 4, 5, 6 };
+
+            var result = numbers.Aggregate((x, y) => x + y)
+                .ToString();
+
+            return result;
+        }
+    }
+}
